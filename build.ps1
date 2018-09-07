fef6e5f6087168fb9200e49c2ceb5d6ae9f28b09
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-09-07 15:12:33 +0200

    really try to fix build on windows

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@
 New-Item -Path .\build -ItemType directory -Force
 Set-Location build
 
-cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=$env:VCPKG_DEFAULT_TRIPLET" ..
+cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=$env:VCPKG_DEFAULT_TRIPLET" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build .
 
 Set-Location ..
